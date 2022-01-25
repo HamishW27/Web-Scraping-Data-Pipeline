7140dc4666f9533ce576c0ec0bf1a861d5142369
HamishW27 <HamishW@protonmail.ch>
2022-01-25 11:34:36 +0000

    Optimise page loading times

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ import time
 
 if __name__ == "__main__":
     driver = webdriver.Chrome()
-    url = 'https://www.epicgames.com/store/en-US/browse?sortBy=releaseDate&sortDir=DESC&count=40'
+    url = 'https://www.epicgames.com/store/en-US/browse?sortBy=releaseDate&sortDir=DESC&count=1000'
     driver.get(url)
+
     time.sleep(5)
 
     accept_cookies_button = driver.find_element_by_xpath(
         '//*[@id="onetrust-accept-btn-handler"]')
     accept_cookies_button.click()
+
+    time.sleep(15)
 
     game_list = driver.find_elements_by_xpath('//*[@class="css-1jx3eyg"]')
 

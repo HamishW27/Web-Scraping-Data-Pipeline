27790817210490617f285055e743a4257993821f
HamishW27 <HamishW@protonmail.ch>
2022-02-02 20:11:10 +0000

    Remove redundant code

--- main.py
+++ main.py
@@ -8,14 +8,11 @@ it contains the Scraper class which allows you to:
 - Monitor trends and changes to the store and its games over time
 '''
 
-from platform import release
-import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
 import uuid
 from datetime import datetime
-import os
 import json
 from pathlib import Path
 
@@ -25,7 +22,8 @@ class Scraper:
     This is the Scraper function. It is used to scrape urls from the
     site's homepage and then information about each individual game
     Attribute:
-        options(Options): A variable that only serves the purpose of allowing Selenium to run headless
+        options(Options): A variable that only serves the purpose of allowing
+        Selenium to run headless
         webpage(string): The url of the page you want to scrape
         driver(WebDriver): Used to specify the browser for selenium to use
         links(List): A list of urls pertaining to the webpage for each game
@@ -35,8 +33,8 @@ class Scraper:
         '''
         See help(Scraper) for accurate signature
         '''
-        # options = Options()
-        # options.headless = True
+        options = Options()
+        options.headless = True
         self.webpage = webpage
         self.driver = webdriver.Chrome()
         self.links = []
@@ -84,16 +82,12 @@ class Scraper:
     @staticmethod
     def scrape_page_info(url, driver):
         driver.get(url)
-        # time.sleep(10)
-        # if driver.current_url.startswith('https://www.epicgames.com/id/login'):
-        #     driver.back()
-        time.sleep(2)
         try:
             age_limit_button = driver.find_element_by_xpath(
                 '//*[@data-component="BaseButton"]')
             age_limit_button.click()
             time.sleep(10)
-        except:
+        except Exception:
             pass
         title = driver.find_element_by_xpath('//h1').text
         price_layout = driver.find_element_by_xpath(
@@ -104,29 +98,34 @@ class Scraper:
             discount, reduced_from_price, price = price_layout
         try:
             sidebar = driver.find_element_by_xpath(
-                '//*[@data-component="SidebarMetadataLayout"]').text.split('\n')
+                '//*[@data-component="SidebarMetadataLayout"]'
+            ).text.split('\n')
             developer = sidebar[sidebar.index('Developer') + 1]
             genre = driver.find_element_by_xpath(
                 '//*[@data-component="Metadata"]').text.split('\n')[1:]
             release_date = sidebar[sidebar.index('Release Date') + 1]
             release_date_as_datetime = datetime.strptime(
                 release_date, '%m/%d/%y')
-        except:
+        except Exception:
             developer, genre, release_date = None, None, None
 
         try:
             critic_bar = driver.find_element_by_xpath(
-                '//*[@data-component="PDPCriticReviewMetricsLayout"]').text.split('\n')
+                '//*[@data-component="PDPCriticReviewMetricsLayout"]'
+            ).text.split('\n')
             critic_recommend = parse_percentage(
                 critic_bar[critic_bar.index('Critics Recommend') - 1])
             critic_top_average = critic_bar[critic_bar.index(
                 'Top Critic Average') - 1]
-        except:
-            critic_recommend, critic_top_average = None, None
+        except Exception:
+            critic_recommend = None
+            critic_top_average = None
 
-        return {'title': title, 'discounted from price': reduced_from_price, 'price': price,
-                'developer': developer, 'genre': genre, 'release date': release_date_as_datetime,
-                'critics recommend': critic_recommend, 'critic top average': critic_top_average}
+        return {'title': title, 'discounted from price': reduced_from_price,
+                'price': price, 'developer': developer, 'genre': genre,
+                'release date': release_date_as_datetime,
+                'critics recommend': critic_recommend,
+                'critic top average': critic_top_average}
 
 
 def parse_percentage(str):
@@ -134,8 +133,8 @@ def parse_percentage(str):
 
 
 if __name__ == "__main__":
-    epicgames = Scraper(
-        'https://www.epicgames.com/store/en-US/browse?sortBy=releaseDate&sortDir=DESC&count=1000')
+    eg_url = 'https://www.epicgames.com/store/en-US/browse?sortBy=releaseDate&sortDir=DESC&count=1000'
+    epicgames = Scraper(eg_url)
     list_of_games = epicgames.get_links()
     print('Finished generating links')
 
@@ -144,21 +143,6 @@ if __name__ == "__main__":
         url_data.append({'url': list_of_games[i], 'id': str(uuid.uuid4())})
 
     Path('./raw_data').mkdir(parents=True, exist_ok=True)
-
-    # print('Logging In')
-    # epicgames.driver.get('https://www.epicgames.com/id/login/epic')
-    # time.sleep(5)
-    # username = epicgames.driver.find_element_by_id(
-    #     'email')
-    # password = epicgames.driver.find_element_by_id(
-    #     'password')
-    # sign_in = epicgames.driver.find_element_by_id(
-    #     'sign-in')
-    # username.send_keys('')
-    # password.send_keys('')
-    # sign_in.click()
-
-    # time.sleep(10)
 
     for i in range(len(url_data)):
         Path('./raw_data/' + str(url_data[i]['id'])

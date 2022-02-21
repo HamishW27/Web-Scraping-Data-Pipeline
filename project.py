5290efb7d71d6587e2c4798870d0fd0a53363466
HamishW27 <HamishW@protonmail.ch>
2022-02-21 20:12:56 +0000

    Add uuid to json file

--- project.py
+++ project.py
@@ -105,7 +105,6 @@ class Scraper:
         html = requests.get(url).text
         page = BeautifulSoup(html, 'html.parser')
 
-        # %%
         uuid = next(item for item in id_links if item["url"] == url)['id']
 
         # Scrape the title and the price changes individually
@@ -182,7 +181,7 @@ class Scraper:
             pictures = []
 
         # Return a dictionary of all useful page details
-        game_dict = {'uuid': uuid,'title': title, 'discounted from price': reduced_from_price,
+        game_dict = {'uuid': uuid, 'url': url, 'title': title, 'discounted from price': reduced_from_price,
                 'price': price, 'developer': developer, 'publisher': publisher,
                 'genre': genre_list, 'release date': release_date_as_datetime,
                 'critics recommend': critic_recommend,
